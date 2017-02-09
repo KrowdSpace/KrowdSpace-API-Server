631a6cbf33c3504eb1cdca30a626e491ac7fe34a
Ben Otter <benotter@me.com>
2017-02-09 14:30:51 -0800

    Added in Socials and Impressions
    Hardcoded in test values for Socials and Impressions page, will set up
    to be random later.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import * as restify from 'restify';
-import {user, proj} from './lib/user_test.js';
+import {user, proj, socials, impressions} from './lib/user_test.js';
 
 const conf = {
     port: 8080,
@@ -7,10 +7,10 @@ const conf = {
 
 const server = restify.createServer();
 
-console.log(user, proj);
-
 server.get(user.userUrl, user.getUser);
 server.get(proj.projUrl, proj.getProj);
+server.get(socials.socUrl, socials.getSocials);
+server.get(impressions.impUrl, impressions.getImp);
 
 server.listen(conf.port, ()=>
 {
--- src/lib/user_test.js
+++ src/lib/user_test.js
@@ -10,7 +10,7 @@ function getUser(req, res, n)
     let usrNum = req.params.usrnum;
 
     let returnObj = {
-        someValue: doSomthingRandom(usrNum)
+        someValue: doSomethingRandom(usrNum)
     };
 
     res.end(JSON.stringify(returnObj));
@@ -27,7 +27,7 @@ function getProj(req, res, n)
     let projNum = req.params.projnum;
 
     let returnObj = {
-        someValue: doSomthingRandom(usrNum + projNum)
+        someValue: doSomethingRandom(usrNum + projNum)
     };
 
     res.end(JSON.stringify(returnObj));
@@ -36,11 +36,56 @@ function getProj(req, res, n)
     n();
 };
 
-function doSomthingRandom(a1)
+function doSomethingRandom(a1)
 {
     return a1;
+};
+
+//Get User's Social details
+const socUrl = '/users/:usernum/socials';
+function getSocials(req, res, n)
+{
+    let usrNum = req.params.usrnum;
+
+    let retObj = Socials;
+
+    res.end(JSON.stringify(retObj));
+
+    //Restify's API's method of moving to next request
+    n();
 }
+
+//Get User's Impression Details
+const impUrl = '/users/:usrnum/impressions';
+function getImp(req, res, n)
+{
+    let usrNum = req.params.usrnum;
+
+    let retObj = SocialGraph;
+
+    res.end(JSON.stringify(retObj));
+
+    //Restify's API's method of moving to next request
+    n();
+}
+
+const Socials = [
+    {views: '9001', project: 'Poject Views', percentage: '3'},
+    {views: '85', project: 'Poject Clicks', percentage: '3'},
+    {views: '23', project: 'Poject Backers', percentage: '34'},
+    {views: '4567', project: 'Poject Support', percentage: '12'},
+    {views: '8800', project: 'Social Media Reach', percentage: '34'},
+];
+
+const SocialGraph = [
+    {alternate: 'odd', title: "Facebook", color: "aero", performance: '30', impressions: '500', reactions: '30', ctr: '5', conversion:'4'},
+    {alternate: 'even', title: "Twitter", color: "purple", performance: '10', impressions: '657', reactions: '8', ctr: '5', conversion:'3'},
+    {alternate: 'odd', title: "Instagram", color: "red", performance: '15', impressions: '754', reactions: '2', ctr: '5', conversion:'3'},
+    {alternate: 'even', title: "Pinterest", color: "green", performance: '45', impressions: '845', reactions: '6', ctr: '5', conversion:'1'}
+];
 
 //Exports deets as objects, should return an object under normal node 'require'
 export const user = {userUrl, getUser};
 export const proj = {projUrl, getProj};
+export const socials = {socUrl, getSocials};
+export const impressions = {impUrl, getImp};
