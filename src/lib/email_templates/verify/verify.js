0aef692f5aecd97877d074a9f5912db920dc09cb
Ben Otter <benotter@me.com>
2017-04-27 18:52:10 -0700

    Adjusted Email Template

--- src/lib/email_templates/verify/verify.js
+++ src/lib/email_templates/verify/verify.js
@@ -27,16 +27,16 @@ export default class VerifyEMT extends MailTemplate
                             </tr>
                             <tr>
                                 <td style="font-size: 14px; font-family: Arial;">
-                                    <p style="margin: 20px 5px 0px 5px;">Hi ${d.fname},</p>
-                                    <p style="margin: 10px 5px;">This message is to confirm that the Krowdspace account with
+                                    <p style="margin: 30px 5px 0px 5px;">Hi ${d.fname},</p>
+                                    <p style="margin: 20px 5px;">This message is to confirm that the Krowdspace account with
                                         the username ${d.username} belongs to you. Verifying your email address will provide access to our Krowdspace Dashboard.</p>
-                                    <p style="text-align: center; margin: 20px 5px 0px 5px;">Please
+                                    <p style="text-align: center; margin: 40px 5px 0px 5px;">Please
                                         confirm your Krowdspace account by clicking the button below:</p>
                                 </td>
                             </tr>
                             <tr>
                                 <td style="font-size: 14px; font-family: Arial; text-align: center;">
-                                    <strong><a href="https://www.krowdspace.com/account/verify.html?$verify_code=${d.verify_code}" style="background-color: #fed136; color: #fff; padding: 12px; display:inline-table; border-radius: 5px; margin: 15px 0px 0px 0px;
+                                    <strong><a href="https://www.krowdspace.com/account/verify.html?$verify_code=${d.verify_code}" style="background-color: #fed136; color: #fff; padding: 12px; display:inline-table; border-radius: 5px; margin: 25px 0px 15px 0px;
                                         text-decoration: none;">CONFIRM ACCOUNT</a></strong>
                                 </td>
                             </tr>
