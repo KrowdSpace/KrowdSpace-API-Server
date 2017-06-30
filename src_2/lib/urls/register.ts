3ba4c3659da73f07d107ee7a1338beea66148911
Ben Otter <benotter@me.com>
2017-06-30 15:14:12 -0700

    Again, you get the picture.

--- src_2/lib/urls/register.ts
+++ src_2/lib/urls/register.ts
@@ -200,7 +200,7 @@ export class RegisterProjectURL extends RestURL implements RestURL
         while(escape++ < 5 || !webData.title.content);
 
         if(!webData.title.content)
-            return this.end(rest, {success: false, data: {web_data_error: true, webData, rawWData}});
+            return this.end(rest, { success: false, data: {web_data_error: true, webData, rawWData, url }});
             
         let coupon_code = crypto.randomBytes(6).toString('base64');
         let unique_id = crypto.randomBytes(10).toString('base64').split('').slice(0,6).join('');
