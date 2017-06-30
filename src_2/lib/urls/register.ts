0681e30a85bbf4b4c72675f990dc3a1ee32ae0f2
Ben Otter <benotter@me.com>
2017-06-30 13:16:30 -0700

    Debug Stuff

--- src_2/lib/urls/register.ts
+++ src_2/lib/urls/register.ts
@@ -196,7 +196,7 @@ export class RegisterProjectURL extends RestURL implements RestURL
         let webData = this.getKSURLData(rawWData, scrapeProfile);
 
         if(!webData.title.content)
-            return this.end(rest, {success: false, data: {web_data_error: true}});
+            return this.end(rest, {success: false, data: {web_data_error: true, webData}});
             
         let coupon_code = crypto.randomBytes(6).toString('base64');
         let unique_id = crypto.randomBytes(10).toString('base64').split('').slice(0,6).join('');
