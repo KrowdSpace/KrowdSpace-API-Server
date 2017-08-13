4fe48eca381d929b3064ce426e306e22017cd30c
Ben Otter <benotter@me.com>
2017-08-13 15:21:50 -0700

    Updated Stats

--- src_2/lib/urls/stats.ts
+++ src_2/lib/urls/stats.ts
@@ -1,4 +1,13 @@
 import {RestURL, safeJSON} from '@otter-co/ottlib';
+import * as superE from '@otter-co/ottlib';
+
+namespace extras 
+{
+    export namespace mongodb_extra 
+    {
+        export class MongoDBDataGetter extends superE.extras.mongodb_extra.MongoDBDataGetter {};
+    }
+}
 
 export class StatsURL extends RestURL implements RestURL
 {
@@ -11,14 +20,24 @@ export class StatsURL extends RestURL implements RestURL
             totalRewards = 0,
             totalRaised = 0;
         
-        let projG = this.dataG['projects_getter'];
+        extras.mongodb_extra.MongoDBDataGetter;
+        
+        let projG = <extras.mongodb_extra.MongoDBDataGetter> this.dataG['projects_getter'];
+
+        let pCount = await projG.collection.count({});
 
         let projR = await projG.aggregate([
             {$match: {}},
-            {$group: {_id: "$platform"}, }
-        ]).catch(err=>err);
+            {$group: {
+                _id: "$platform",
+                raisedAmount: { $sum: "$project_data.meta_data.raised" },
+                rewardAmmount: { $sum: "$project_data.info_data.reward_ammount" }
+            }, }
+        ]);
         
-        this.end(rest, {success: true, data: { totalProjects, totalRewards, totalRaised } });
+        totalProjects = pCount;
+
+        this.end(rest, {success: true, data: { totalProjects, projR} });
     }
 }
 
