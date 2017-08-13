9b8f5a04b9ba53ce1b2811d8ffcae638aade8d6c
Ben Otter <benotter@me.com>
2017-08-13 16:29:57 -0700

    Eyup

--- src_2/lib/data_templates/projects.ts
+++ src_2/lib/data_templates/projects.ts
@@ -7,15 +7,12 @@ export class ProjectsGetter extends extras.mongodb_extra.MongoDBDataGetter
 
     public agg(data: any)
     {
-        return new Promise((ressolve, reject)=>
+        return new Promise((resolve, reject)=>
         {
             this.aggregate(data, (err, res, f)=>
             {
                 if(!err)
-                    res.toArray((err, docs)=>
-                    {
-                        ressolve({success: true, data: docs})
-                    });
+                    resolve({success: true, data: res})
                 else
                     reject({success: false, data: err});
             });
